802bda251f7722e6548c36639e87a996a613f10f
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-05-08 01:41:25 +0600

    solved problem 1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 // !Problem 1
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper || typeof toUpper === "undefined") {
+    return input.toUpperCase();
+  }
+  if (toUpper === false) {
+    return input.toLowerCase();
+  }
+  //   return "hello";
+}
+const p1 = formatString("Hello", false);
+console.log(p1);
